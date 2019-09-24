88ac6691af7adb99e249bb744912f4d0b84d13e8
vumiquang <vuminhquang.dsn@gmail.com>
2019-09-24 10:13:11 +0700

    fix somethings

--- validate_form/js/main.js
+++ validate_form/js/main.js
@@ -2,7 +2,7 @@ function makeDay(){
     var optionDay = "";
     for(let i = 1;i <= 31; i++)
         optionDay += "<option  value="+i+">"+i+"</option>";
-        optionDay += "<option value=\"-1\" selected=\"1\">"+"Ngày"+"</option>";
+        optionDay += "<option value=\"-1\" selected=\"1\">"+"Day"+"</option>";
     $("#day").html(optionDay);
 }
 
@@ -10,7 +10,7 @@ function makeMonth(){
     var optionMonth = "";
     for(let i = 1;i <= 12; i++)
         optionMonth += "<option value="+i+">"+i+"</option>";
-        optionMonth += "<option value=\"-1\" selected=\"1\" >"+"Tháng"+"</option>";
+        optionMonth += "<option value=\"-1\" selected=\"1\" >"+"Month"+"</option>";
     $("#month").html(optionMonth);
 }
 
@@ -18,7 +18,7 @@ function makeYear(){
     var optionYear = "";
     for(let i = 1905;i <= 2019; i++)
         optionYear += "<option value="+i+">"+i+"</option>";
-        optionYear += "<option value=\"-1\" selected=\"1\">"+"Năm"+"</option>";
+        optionYear += "<option value=\"-1\" selected=\"1\">"+"Year"+"</option>";
     $("#year").html(optionYear);
 }
 
@@ -30,76 +30,97 @@ $(document).ready(function(){
 
 function checkValidate(){
     $(".warner").css("display","none");
-
+    var check = 0;
+    //kiểm tra tài khoản
     if($("#account").val() == "") {
         $("#warner-account").css("display","block");
         $("#warner-account").html("Không được để trống");
-        return false;
+        check++;
     }
     else if($("#account").val().length >= 20) {
         $("#warner-account").css("display","block");
         $("#warner-account").html("Tài khoản ít hơn 20 ký tự");
-        return false;
+        check++;
     }
     else if(/\s/.test($("#account").val())) {
         $("#warner-account").css("display","block");
         $("#warner-account").html("Không chứa dấu cách");
-        return false;
+        check++;
     }
+
+    //kiểm tra password
     if($("#password").val() == "") {
         $("#warner-password").css("display","block");
         $("#warner-password").html("Không được để trống");
-        return false;
+        check++;
     }
     else if($("#password").val().length < 6) {
         $("#warner-password").css("display","block");
         $("#warner-password").html("Mật khẩu phải lớn hơn 6 ký tự");
-        return false;
+        check++;
     }
     else if(/\s/.test($("#password").val())) {
         $("#warner-password").css("display","block");
         $("#warner-password").html("Không chứa dấu cách");
-        return false;
+        check++;    
     }
+
+    //kiểm tra giới tính
+    if(document.forms['sign-up']['gender'].value == ""){
+        $('#warner-gender').css('display','block');
+        $('#warner-gender').html('Chưa đánh dấu');
+        check++;
+    }
+
+    //kiểm tra ngày/tháng năm
     if($("#day :selected").val() == "-1") {
         $("#warner-date").css("display","block");
         $("#warner-date").html("Chọn thời gian hợp lệ");
-        return false;
+        check++;
     }
     if($("#month :selected").val() == "-1") {
         $("#warner-date").css("display","block");
         $("#warner-date").html("Chọn thời gian hợp lệ");
-        return false;
+        check++;
     }
     if($("#year :selected").val() == "-1") {
         $("#warner-date").css("display","block");
         $("#warner-date").html("Chọn thời gian hợp lệ");
-        return false;
+        check++;
     }
+
+    //kiểm tra địa chỉ
     if($("#address").val() == "") {
         $("#warner-address").css("display","block");
         $("#warner-address").html("Không được để trống");
-        return false;
+        check++;
     }
+
+    // kiểm tra số điện thoại
     if($("#phone").val() == "") {
         $("#warner-phone").css("display","block");
         $("#warner-phone").html("Không được để trống");
-        return false;
+        check++;
     }
     else if(/\D/.test($("#phone").val())) {
         $("#warner-phone").css("display","block");
         $("#warner-phone").html("Phải là số");
-        return false;
+        check++;
     }
+
+
+    //kiểm tra email
     if($("#email").val() == "") {
         $("#warner-email").css("display","block");
         $("#warner-email").html("Không được để trống");
-        return false;
+        check++;
     }
     else if(/\s/.test($("#email").val())) {
         $("#warner-email").css("display","block");
         $("#warner-email").html("Không chứa dấu cách");
-        return false;
+        check++;
     }
-    return true;
+    if(check == 0)
+     return true;
+     else return false;
 }
