a105c9424e2f25d0667576580421a23ea0191f90
vumiquang <vuminhquang.dsn@gmail.com>
2019-09-29 19:44:51 +0700

    them vai tinh nang

--- Table/js/main.js
+++ Table/js/main.js
@@ -37,65 +37,29 @@ var students = [
 }
 ];
 
-//xắp sếp danh sách tăng theo tên
-function sortNameIncreaseStudents(){
+//sort theo dữ liệu cột truyền vào
+function sortStudents(nameColumn,sign){
     students.sort(function(a,b){
-        let typeA = a.name.toUpperCase();
-        let typeB = b.name.toUpperCase();
-        let compare = 0;
-        if(typeA > typeB)
-            compare = 1;
-        if(typeA < typeB)
-            compare = -1;
-        return compare;
-
-    })
-}
-
-//xắp sếp danh sách giảm theo tên
-function sortNameReductionStudents(){
-    students.sort(function(a,b){
-        let typeA = a.name.toUpperCase();
-        let typeB = b.name.toUpperCase();
-        let compare = 0;
-        if(typeA < typeB)
-            compare = 1;
-        if(typeA > typeB)
-            compare = -1;
-        return compare;
-
-    })
-}
-
-//xắp sếp danh sách giảm theo năm sinh
-function sortReductionStudents(){
-    sortIncreaseStudents();
-    students.reverse();
-}
-
-//xắp sếp theo tên tăng theo năm sinh
-function sortIncreaseStudents(){
-    stdLength = students.length;
-    for(let i = 0; i < (stdLength -1) ; i++)
-        for(let j = i+1 ;j < stdLength;j++)
-          if(Number(students[i].birthday) > Number(students[j].birthday)){
-            let temp = students[i];
-            students[i] = students[j];
-            students[j] = temp;
-          }
+        if(a[nameColumn].toUpperCase() > b[nameColumn].toUpperCase())
+            return -1*sign;
+        else if(a[nameColumn].toUpperCase() < b[nameColumn].toUpperCase())
+            return 1*sign;
+        else
+            return 0;
+    });
 }
 
 //in danh sách ra màn hình
-function renderStudent(){
+function renderStudent(stdList){
     $("#table-title").nextAll().remove();
-    stdLength = students.length;
-    html = "";
+    stdLength = stdList.length;
+    let html = "";
     for(let i = 0; i < stdLength ; i++){
         html += "<tr>";
-        html += "<td class=\"name\">"+students[i].name+"</td>";
-        html += "<td class=\"birthday\">"+students[i].birthday+"</td>";
-        html += "<td class=\"mail\">"+students[i].email+"</td>";
-        html += "<td class=\"phone\">"+students[i].phone+"</td>";
+        html += "<td class=\"name\">"+stdList[i].name+"</td>";
+        html += "<td class=\"birthday\">"+stdList[i].birthday+"</td>";
+        html += "<td class=\"mail\">"+stdList[i].email+"</td>";
+        html += "<td class=\"phone\">"+stdList[i].phone+"</td>";
         html += "<td>";
         html += "<div class=\"edit\">";
         html += "<i class=\"fas fa-edit\"></i>";
@@ -109,78 +73,52 @@ function renderStudent(){
         html += "</td></tr>";
     }
     $("#tbe-students").append(html);
-}
-renderStudent();
-
-//lấy dữ liệu từ bảng vào biên students
-function getTable(){   
-    students = [];
-    elementTable = document.getElementById("tbe-students").rows;
-
-    for(let i = 1;i < elementTable.length; i++){
-        let student = {
-            name:"",
-            birthday:"",
-            email:"",
-            phone:"",
-        };
-
-        student.name = elementTable[i].cells[0].innerHTML;
-        student.birthday = elementTable[i].cells[1].innerHTML;
-        student.email = elementTable[i].cells[2].innerHTML;
-        student.phone = elementTable[i].cells[3].innerHTML;
-
-        students.push(student);
-    }
-}
-
-// gán sự kiện tăng giảm cho nút
-$('#sort-icon').click(function(){
-    $('#sort-icon').toggleClass("active");
-    if($('#sort-icon').attr("class").includes("active")){
-        sortReductionStudents();
-        $('#down-icon').css("color","rgba(255,255,255,1)");
-        $('#up-icon').css("color","rgba(255,255,255,0.2)");
-    }
-    else{
-        sortIncreaseStudents();
-        $('#up-icon').css("color","rgba(255,255,255,1)");
-        $('#down-icon').css("color","rgba(255,255,255,0.2)");
-    }
-    $('#name-down-icon').css("color","rgba(255,255,255,0.2)");
-    $('#name-up-icon').css("color","rgba(255,255,255,0.2)");
-    renderStudent();
+    $('#add').show();
+    editData();
     deleteRow();  
+}
+renderStudent(students);
+
+//Hàm sort các cột
+$('.sort').click(function(){
+    let nameColumn = $(this).attr("id");
+    let sign;
+    $('.arrow-icon').css("color","rgba(255,255,255,0.2)");
+    $(this).toggleClass("active");
+    if($(this).attr("class").includes("active")){
+        sign = -1;
+        $(this).children().first().css("color","rgba(255,255,255,1)");
+    }
+        
+    else {
+        sign = 1;
+        $(this).children().last().css("color","rgba(255,255,255,1)");
+    }
+    sortStudents(nameColumn,sign);
+    renderStudent(students);
 });
 
-// gán sự kiện tăng giảm cho nút sắp xếp tên
-$('#name-sort-icon').click(function(){
-    $('#name-sort-icon').toggleClass("active");
-    if($('#name-sort-icon').attr("class").includes("active")){
-        sortNameReductionStudents();
-        $('#name-down-icon').css("color","rgba(255,255,255,1)");
-        $('#name-up-icon').css("color","rgba(255,255,255,0.2)");
-    }
-    else{
-        sortNameIncreaseStudents();
-        $('#name-up-icon').css("color","rgba(255,255,255,1)");
-        $('#name-down-icon').css("color","rgba(255,255,255,0.2)");
-    }
-    $('#up-icon').css("color","rgba(255,255,255,0.2)");
-    $('#down-icon').css("color","rgba(255,255,255,0.2)");
-    renderStudent();
-    deleteRow();  
-});
+
+
 
 //xóa row table
 function deleteRow(){
     $('.delete').click(function(){
         $('#confirm-delete').css("display","block");
         let thisRow = this ;
+        thisRow = $(thisRow).parent().parent();
+
+        let nameValue = thisRow.children()[0].innerHTML;
+        let birthdayValue = thisRow.children()[1].innerHTML;
+        let emailValue = thisRow.children()[2].innerHTML;
+        let phoneValue = thisRow.children()[3].innerHTML;
+        
         $('#yes-delete').click(function(){
             $('#confirm-delete').css("display","none");
-            $(thisRow).parent().parent().remove();
-            getTable();
+            thisRow.remove();
+            students = students.filter(function(std){
+                return !(std["name"] == nameValue && std["birthday"] == birthdayValue && std["email"] == emailValue && std["phone"] == phoneValue);
+            });
         })
         $('#no-delete').click(function(){
             $('#confirm-delete').css("display","none");
@@ -188,4 +126,129 @@ function deleteRow(){
     });
 };
 
-deleteRow();
+//Sửa giá trị
+
+function editData(){
+        $('.edit').click(function(){
+            let nodeCurrents = $(this).parent().siblings();
+            let nameCurrent = nodeCurrents[0].innerHTML;
+            let birthdayCurrent = nodeCurrents[1].innerHTML;
+            let emailCurrent = nodeCurrents[2].innerHTML;
+            let phoneCurrent = nodeCurrents[3].innerHTML;
+            let indexCurrent = students.findIndex(function(st){ 
+                return (st.name == nameCurrent && st.birthday == birthdayCurrent && st.email == emailCurrent && st.phone == phoneCurrent)
+            });
+            nodeCurrents[0].innerHTML = "<input style=\"height:25px;line-height:25px;padding:0 5px;\" type=\"text\" id=\"new-name\" value=\""+nameCurrent+"\">"
+            nodeCurrents[1].innerHTML = "<input style=\"height:25px;line-height:25px;padding:0 5px;\" type=\"text\" id=\"new-birthday\" value=\""+birthdayCurrent+"\">"
+            nodeCurrents[2].innerHTML = "<input style=\"height:25px;line-height:25px;padding:0 5px;\" type=\"text\" id=\"new-email\" value=\""+emailCurrent+"\">"
+            nodeCurrents[3].innerHTML = "<input style=\"height:25px;line-height:25px;padding:0 5px;\" type=\"text\" id=\"new-phone\" value=\""+phoneCurrent+"\">"
+        
+            $(this).parents("td").html("<span class=\"save\">Save</span>");
+            saveData(indexCurrent);
+        })
+}
+
+//save 
+function saveData(index){
+    $('.save').click(function(){
+        let std = {
+            name : "",
+            birthday : "",
+            email : "",
+            phone : ""
+        }
+       
+        std.name =  $($(this).parent().siblings()[0]).children().val();
+        std.birthday =  $($(this).parent().siblings()[1]).children().val();
+        std.email =  $($(this).parent().siblings()[2]).children().val();
+        std.phone =  $($(this).parent().siblings()[3]).children().val();
+        students.splice(index,1,std);
+        renderStudent(students);
+    })
+}
+
+//Tìm kiếm giá trị 
+$('#search-icon').click(function(){
+    let searchText = new RegExp(removeAccents($('#search-text').val()),'i');
+    let searchType =  $('#search-type').val();
+    let foundListStudents = [];
+    foundListStudents = students.filter(function(std){
+        return searchText.test(removeAccents(std[searchType]));
+    });
+    renderStudent(foundListStudents);
+    if($('#search-text').val() == "")
+         $('#add').show();
+    else
+        $('#add').hide();
+})
+
+
+
+//chuyển có dấu về không có dấu
+function removeAccents(str) {
+    var AccentsMap = [
+      "aàảãáạăằẳẵắặâầẩẫấậ",
+      "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
+      "dđ", "DĐ",
+      "eèẻẽéẹêềểễếệ",
+      "EÈẺẼÉẸÊỀỂỄẾỆ",
+      "iìỉĩíị",
+      "IÌỈĨÍỊ",
+      "oòỏõóọôồổỗốộơờởỡớợ",
+      "OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ",
+      "uùủũúụưừửữứự",
+      "UÙỦŨÚỤƯỪỬỮỨỰ",
+      "yỳỷỹýỵ",
+      "YỲỶỸÝỴ"    
+    ];
+    for (var i=0; i<AccentsMap.length; i++) {
+      var re = new RegExp('[' + AccentsMap[i].substr(1) + ']', 'g');
+      var char = AccentsMap[i][0];
+      str = str.replace(re, char);
+    };
+    return str;
+  }
+
+  // thêm dữ liệu
+  $('#add').click(function(){
+    //chỉ cho phép thêm 1 dữ liệu 1
+    if(!$(this).attr("class").includes("add-data")){
+    $('#add').hide();
+    let html = "";
+        html += "<tr>";
+        html += "<td class=\"name\">"+"<input class=\"add-name\" style=\"height:20px;line-height:20px;padding: 0 5px;\"></input>"+"</td>";
+        html += "<td class=\"birthday\">"+"<input class=\"add-birthday\" style=\"height:20px;line-height:20px;padding: 0 5px;\"></input>"+"</td>";
+        html += "<td class=\"mail\">"+"<input class=\"add-email\" style=\"height:20px;line-height:20px;padding: 0 5px;\"></input>"+"</td>";
+        html += "<td class=\"phone\">"+"<input class=\"add-phone\" style=\"height:20px;line-height:20px;padding: 0 5px;\"></input>"+"</td>";
+        html += "<td class=\"add-finish\">";
+        html += "<i class=\"fas fa-check\"></i>" ;
+        html += "<span>Xong</span>";
+        html += "</td></tr>";
+    $("#tbe-students").append(html);
+    $(this).addClass("add-data");
+    addFinish();
+    };
+  });
+
+
+  function addFinish(){
+      $('.add-finish').click(function(){
+        let nameValue = $($(this).siblings()[0]).children().val();
+        let birthdayValue = $($(this).siblings()[1]).children().val();
+        let emailValue = $($(this).siblings()[2]).children().val();
+        let phoneValue = $($(this).siblings()[3]).children().val();
+        let std = {
+            name : nameValue,
+            birthday : birthdayValue,
+            email : emailValue,
+            phone : phoneValue
+            }
+        students.push(std);
+        renderStudent(students);
+        $('#add').removeClass("add-data");
+        $('#add').show();
+      });
+  }
+
+
+
