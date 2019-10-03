430d1ee975a9379813756061947fc77fba363fcc
vumiquang <vuminhquang.dsn@gmail.com>
2019-10-03 22:56:25 +0700

    add folder boostrap

---
+++ boostrap-4/Toasts/js/main.js
@@ -0,0 +1,25 @@
+var displayToast = function(){
+    document.getElementsByClassName('toast')[0].style.display = 'block';
+    document.getElementsByClassName('toast-icon-close')[0].addEventListener('click',closeToast);
+    timingClose();
+};
+
+document.getElementById('btn-toast').addEventListener('click',displayToast);
+
+var timingClose = function(){
+    document.getElementById('toast-time-close').innerText =  `5 seconds ago` ;
+    var x = 4;
+    var myTimerClose = setInterval(function(){
+        document.getElementById('toast-time-close').innerText = x + ` seconds ago` ;
+        x-=1;
+        if(x == -1){
+            clearTimeout(myTimerClose);
+            document.getElementsByClassName('toast')[0].style.display = 'none';
+        }
+    },999);
+}
+
+var closeToast = function(){
+    document.getElementsByClassName('toast')[0].style.display = 'none';
+}
+
