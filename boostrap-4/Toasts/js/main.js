b1ee0b7f6cd00b2a57eb71d87997eb46cd071f27
vumiquang <vuminhquang.dsn@gmail.com>
2019-10-06 22:30:26 +0700

    fix toast

--- boostrap-4/Toasts/js/main.js
+++ boostrap-4/Toasts/js/main.js
@@ -9,17 +9,22 @@ document.getElementById('btn-toast').addEventListener('click',displayToast);
 var timingClose = function(){
     document.getElementById('toast-time-close').innerText =  `5 seconds ago` ;
     var x = 4;
+    document.getElementById('btn-toast').removeEventListener('click',displayToast);
     var myTimerClose = setInterval(function(){
         document.getElementById('toast-time-close').innerText = x + ` seconds ago` ;
         x-=1;
         if(x == -1){
-            clearTimeout(myTimerClose);
+            clearInterval(myTimerClose);
             document.getElementsByClassName('toast')[0].style.display = 'none';
+            document.getElementById('btn-toast').addEventListener('click',displayToast);
         }
     },999);
 }
 
 var closeToast = function(){
+    for (var i = 1; i < 99999; i++)
+        window.clearInterval(i);
+    document.getElementById('btn-toast').addEventListener('click',displayToast);
     document.getElementsByClassName('toast')[0].style.display = 'none';
 }
 
